2ea33e97713eddf60f841853a442600c0bbdc6c2
Guilherme Nunes <nunesfurtado2007@gmail.com>
2026-04-15 19:39:51 -0300

    update: organiza dependencias. update: transfere dados sensíveis do servidor/banco para .env. feat: arquivo base para .env.

--- src/backend/servidor/Conexao.java
+++ src/backend/servidor/Conexao.java
@@ -1,16 +1,25 @@
 package backend.servidor;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexao {
-    // Note o final da URL com /mydb
-    private static final String URL = "jdbc:mysql://localhost:3306/mydb";
-    private static final String USER = "root"; 
-    private static final String PASS = "SuaNovaSenha123";
+    // Carrega o arquivo .env
+    private static Dotenv dotenv = Dotenv.load();
+
+    // Busca os valores pelas chaves definidas no arquivo
+    private static final String URL = dotenv.get("DB_URL");
+    private static final String USER = dotenv.get("DB_USER");
+    private static final String PASS = dotenv.get("DB_PASS");
 
     public static Connection conectar() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASS);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Driver não encontrado", e);
+        }
     }
 }
