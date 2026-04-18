07c180a2595449a0c2c62f12da91a367883a370d
Guilherme Nunes <nunesfurtado2007@gmail.com>
2026-04-18 20:55:43 -0300

    update: crud ajuda completo e testado.

--- src/backend/DAO/ajudaDAO/AjudaDAO.java
+++ src/backend/DAO/ajudaDAO/AjudaDAO.java
@@ -10,8 +10,25 @@ import backend.servidor.Conexao;
 //classe
 public class AjudaDAO {
 
+    //metodo para criar ajuda
+    public void criarAjuda(String nome, String descricao){
+        String sql = "INSERT INTO ajuda(nome, descricao) VALUES(?,?)";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+                
+            stmt.setString(1,nome);
+            stmt.setString(2, descricao);
+
+            stmt.executeUpdate();
+
+        }catch(SQLException e){
+            System.err.println("Erro ao criar ajuda: " + e.getMessage());
+        }
+    }
+
     // metodo para pegar "ajuda" do banco de dados e colocar no molde ajuda
-    public Ajuda getAjuda(int id) {
+    public Ajuda getAjuda(int id){
         String sql = "SELECT idAjuda, nome, descricao FROM ajuda WHERE idAjuda = ?";
 
         try(Connection conn = Conexao.conectar();
@@ -33,5 +50,28 @@ public class AjudaDAO {
         return null;
     }
 
-    
+    public void atualizarAjuda(int id, String nome, String descricao){
+        String sql = "UPDATE ajuda SET nome  = ?, descricao = ? WHERE idAjuda = ?";
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setString(1, nome);
+                stmt.setString(2, descricao);
+                stmt.setInt(3, id);
+
+                stmt.executeUpdate();
+            }catch(SQLException e){
+                System.err.println("Erro ao atualizar ajuda: " + e.getMessage());
+            }
+    }
+
+        public void deletarAjuda(int id){
+        String sql = "DELETE FROM ajuda WHERE idAjuda = ?";
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setInt(1,id);
+                stmt.executeUpdate();
+        }catch(SQLException e){
+            System.err.println("Erro ao deletar ajuda: " + e.getMessage());
+        }
+    }
 }
