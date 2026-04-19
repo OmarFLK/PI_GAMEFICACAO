63fd0a0f8c96b5bec5acefd28c95763019ae058b
Guilherme Nunes <nunesfurtado2007@gmail.com>
2026-04-19 06:58:36 -0300

    feat: CRUD da tabela Alternativa está completo e testado.

--- src/App.java
+++ src/App.java
@@ -5,7 +5,7 @@ import frontend.LoginTela;
 //classe main
 public class App {
     public static void main(String[] args) {
-
+        
         // Inicia a interface gráfica (Swing)
         SwingUtilities.invokeLater(() -> {
             try {
--- src/backend/DAO/alternativasDAO/AlternativasDAO.java
+++ src/backend/DAO/alternativasDAO/AlternativasDAO.java
@@ -12,6 +12,23 @@ import backend.servidor.Conexao;
 //classe
 public class AlternativasDAO {
 
+    //criar alternativa
+    public void criarAlternativa(int idPergunta, String texto, String imagemURL, int correta){
+        String sql = "INSERT INTO Alternativa (idPergunta, texto, imagemURL, correta) VALUES(?,?,?,?)";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setInt(1,idPergunta);
+                stmt.setString(2,texto);
+                stmt.setString(3, imagemURL);
+                stmt.setInt(4, correta);
+
+                stmt.executeUpdate();
+        }catch(SQLException e){
+            System.err.println("Erro ao criar alternativa: " + e.getMessage());
+        }
+    }
+
     //metodo para pegar a alternativa do banco por id e colocar no molde
     //da classe alternativa
     public Alternativa getAlternativa(int id) {
@@ -63,5 +80,35 @@ public class AlternativasDAO {
             System.err.println("Erro ao buscar alternativa: " + e.getMessage());
         }
         return lista;
+    }
+
+    //metodo atualizar alternativa
+    public void atualizarAlternativa(int idAlternativa, String texto, String imagemURL, int correta){
+        String sql = "UPDATE Alternativa SET texto = ?, imagemURL = ?, correta = ? WHERE idAlternativa = ?";
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, texto);
+            stmt.setString(2, imagemURL);
+            stmt.setInt(3, correta);
+            stmt.setInt(4,idAlternativa);
+
+            stmt.executeUpdate();
+        }catch(SQLException e){
+            System.err.println("Erro ao atualizar alternativa: " + e.getMessage());
+        }
+    }
+
+    //metodo para deletar alternativa
+    public void deletarAlternativa(int id){
+        String slq = "DELETE FROM Alternativa WHERE idAlternativa = ?";
+
+        try(Connection conn = Conexao.conectar();
+            PreparedStatement stmt = conn.prepareStatement(slq)){
+            stmt.setInt(1,id);
+            
+            stmt.executeUpdate();
+        }catch(SQLException e){
+            System.err.println("Erro ao deletar alternativa: " + e.getMessage());
+        }
     }
 }
