6dbb6f85a8189a5d3f161308dff73d688b0a698e
Guilherme Nunes <nunesfurtado2007@gmail.com>
2026-04-23 11:07:42 -0300

    feat: crud partida completo.

--- src/backend/DAO/partidaDAO/PartidaDAO.java
+++ src/backend/DAO/partidaDAO/PartidaDAO.java
@@ -5,10 +5,28 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+
 import backend.servidor.Conexao;
 
 //classes
 public class PartidaDAO {
+
+    //criar partida
+    public void criarPartida(int idUsuario, int Pontuacao){
+        String sql = "INSERT INTO Partida (idUsuario, Pontuacao) VALUES(?,?)";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+                stmt.setInt(1, idUsuario);
+                stmt.setInt(2, Pontuacao);
+
+                stmt.executeUpdate();
+                
+        }catch(SQLException e){;
+            System.err.println("Erro ao criar partida: " + e.getMessage());
+        }
+    }
     
     //metodo para pegar partidas no banco de dados 
     public Partida getPartida(int id){
@@ -34,4 +52,24 @@ public class PartidaDAO {
             }
         return null;
     }
+
+    // metodo para atualizar partida
+    public void atualizarPontuacao(int idPartida, int novaPontuacao) {
+        String sql = "UPDATE Partida SET Pontuacao = ? WHERE idPartida = ?";
+
+        try (Connection conn = Conexao.conectar();
+             PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setInt(1, novaPontuacao);
+            stmt.setInt(2, idPartida);
+
+            int linhasAfetadas = stmt.executeUpdate();
+            if (linhasAfetadas > 0) {
+                System.out.println("Pontuação da partida " + idPartida + " atualizada com sucesso!");
+            }
+        } catch (SQLException e) {
+            System.err.println("Erro ao atualizar partida: " + e.getMessage());
+        }
+    }
+
 }
